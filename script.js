2a035f645575d6200e7a37dc28109ce5a16458ef
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-05-17 17:22:25 +0100

    Store data in local storage

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const originalLinks = document.querySelector('#original-links');
 const shortLinks = document.querySelector('#short-links');
 const button = document.querySelector('.button');
 const input = document.querySelector('#api-input');
+const linksContainer = document.querySelector("#links-container");
+
+// let urlArray = [];
+// let urlObjectParameter = {
+//   originalLink: "",
+//   shortLink: ""
+// }
+// let urlObject = {};
+let display = "";
+
+// let urlLocalStorage = JSON.parse(localStorage.getItem("url's"))
+// console.log(urlLocalStorage)
+
+let urlLocalStorage = JSON.parse(localStorage.getItem("url's"))
 
 const shortenUrlAddress = () => {
   const data = async (urlvalue) => {
@@ -11,13 +25,29 @@ const shortenUrlAddress = () => {
     const response = await get.json()
     return response
   }
+
   button.addEventListener('click', async (e) => {
     const urlvalue = input.value;
     const { result: { original_link, short_link } } = await data(urlvalue)
-  
-   originalLinks.textContent += `${original_link}`;
-   shortLinks.textContent += `${short_link}`;
+    let urlObjectParameter = {
+      originalLink: original_link,
+      shortLink: short_link
+    }
+    // urlObjectParameter.originalLink = [`${original_link}`];
+    // urlObjectParameter.shortLink = [`${short_link}`];
+    // urlArray.push(urlObjectParameter)
+    localStorage.setItem("url's", JSON.stringify(urlObjectParameter))
+    console.log(localStorage)
+// console.log(urlLocalStorage)
    input.value = "";
+
+  //  for (let i = 0; i < urlObjectParameter.length; i++) {
+    originalLinks.textContent = `${original_link}`
+    shortLinks.textContent = `${short_link}`
+    // console.log(urlLocalStorage)
+  // }
+  
+  // linksContainer.innerHTML = display  
   })
 }
 
